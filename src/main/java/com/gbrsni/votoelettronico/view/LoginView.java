d43de6c31bb856ed9e540a41b9fea0b7875e3600
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-03-03 18:41:50 +0100

    aggiunto file LoginView.java

--- src/main/java/com/gbrsni/votoelettronico/view/LoginView.java
+++ src/main/java/com/gbrsni/votoelettronico/view/LoginView.java
@@ -1,10 +1,28 @@
 package com.gbrsni.votoelettronico.view;
+//import com.gbrsni.votoelettronico.controller.*;
 
-public class LoginView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+public class LoginView extends Application {
 
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("LoginView.fxml"));
+        
+        primaryStage.setTitle("Voto Elettronico");
+        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+       launch(args);
+    
+    }
+    
 }
