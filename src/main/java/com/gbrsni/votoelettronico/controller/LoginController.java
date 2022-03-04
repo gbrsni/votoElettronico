e599d55a1cbf78d54f0517d9fdb4ffde56007515
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-03-04 15:49:30 +0100

    add Items ComboBox

--- src/main/java/com/gbrsni/votoelettronico/controller/LoginController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/LoginController.java
@@ -2,15 +2,30 @@ package com.gbrsni.votoelettronico.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.io.IOException;
+import com.gbrsni.votoelettronico.controller.DashBoardController;
 
+public class LoginController {
+	
+	private Stage stage; 
+	private Scene scene;
+	private Parent root;
+	
     @FXML
     private ResourceBundle resources;
 
@@ -27,7 +42,7 @@ public class LoginController {
     private TextField labelUsername;
 
     @FXML
-    private ComboBox<?> tipologiaUtente;
+    private ComboBox<String> tipologiaUtente ;
 
     @FXML
     void handleName(ActionEvent event) {
@@ -45,8 +60,22 @@ public class LoginController {
     }
 
     @FXML
-    void pressButton(ActionEvent event) {
-
+    void pressButton(ActionEvent event) throws IOException {
+    	
+    	//CAMBIARE LA SCENA -> DASHBOARD.fxml
+    	/**
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("DashBoardView.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        DashBoardController controller = (DashBoardController)
+        stage.show();
+        **/
+    	FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("DashBoardView.fxml"));
+        Parent root = loader.load();
+        DashBoardController controller = loader.getController();
+        //controller.setName("ciao");
+        button.getScene().setRoot(root);
     }
 
     @FXML
@@ -55,7 +84,8 @@ public class LoginController {
         assert labelPassword != null : "fx:id=\"labelPassword\" was not injected: check your FXML file 'LoginView.fxml'.";
         assert labelUsername != null : "fx:id=\"labelUsername\" was not injected: check your FXML file 'LoginView.fxml'.";
         assert tipologiaUtente != null : "fx:id=\"tipologiaUtente\" was not injected: check your FXML file 'LoginView.fxml'.";
-
+        ObservableList<String> dbTypeList = FXCollections.observableArrayList("Elettore","Impiegato");
+        tipologiaUtente.setItems(dbTypeList);
     }
 
 }
