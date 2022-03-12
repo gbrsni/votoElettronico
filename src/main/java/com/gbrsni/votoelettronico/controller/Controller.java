597c7dd9b19575b1fd9e071b5d5a2f3e3e1cd022
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-03-12 14:13:44 +0100

    creazione superclasse

---
+++ src/main/java/com/gbrsni/votoelettronico/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.gbrsni.votoelettronico.controller;
+
+import com.gbrsni.votoelettronico.Home;
+import java.io.IOException;
+
+public abstract class Controller {
+  
+    public void navigate(String view, Object parameter) {
+        try {
+            Home.navigate(this, view, parameter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void navigate(String view) {
+        navigate(view, null);
+    }
+}
