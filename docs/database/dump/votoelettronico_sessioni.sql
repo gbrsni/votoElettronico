5bd4deb568142d148d75e00c2097f9fbcd6cdef6
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-03-14 21:34:32 +0100

    gestione stato sessione

--- docs/database/dump/votoelettronico_sessioni.sql
+++ docs/database/dump/votoelettronico_sessioni.sql
@@ -26,23 +26,14 @@ CREATE TABLE `sessioni` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
   `descrizione` text,
-  `data` date NOT NULL,
-  `modvoto` enum('ORDINALE','CATEGORICO','CATEGORICO_CON_PREFERENZE','REFERENDUM') NOT NULL,
-  `modvittoria` enum('maggioranza','maggioranza assoluta','referendum senza quorum','referendum con quorum') NOT NULL,
-  `stato` enum('CHIUSA','IN_CORSO','CONCLUSA','SCRUTINATA') NOT NULL,
-  `nvoti` int NOT NULL,
+  `data` date DEFAULT NULL,
+  `modvoto` enum('ORDINALE','CATEGORICO','CATEGORICO_CON_PREFERENZE','REFERENDUM') DEFAULT NULL,
+  `modvittoria` enum('MAGGIORANZA','MAGGIORANZA_ASSOLUTA','REFERENDUM_SENZA_QUORUM','REFERENDUM_CON_QUORUM') DEFAULT NULL,
+  `stato` enum('CHIUSA','IN_CORSO','CONCLUSA','SCRUTINATA') DEFAULT NULL,
+  `nvoti` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sessioni`
---
-
-LOCK TABLES `sessioni` WRITE;
-/*!40000 ALTER TABLE `sessioni` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sessioni` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-14 20:08:53
+-- Dump completed on 2022-03-14 21:33:36
