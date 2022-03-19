98176af53c28dab3d19a7d2b897aa445207aa9a4
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-03-19 20:27:11 +0100

    aggiunto controller Avvio Sessione

--- src/main/java/com/gbrsni/votoelettronico/controller/AvvioSessioneController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/AvvioSessioneController.java
@@ -1,13 +1,24 @@
 package com.gbrsni.votoelettronico.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import com.gbrsni.votoelettronico.data_access.SessioneDiVotoDAOImpl;
+import com.gbrsni.votoelettronico.models.Gestore;
+import com.gbrsni.votoelettronico.models.SessioneDiVoto;
+import com.gbrsni.votoelettronico.models.StatoSessione;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class AvvioSessioneController extends Controller{
-
+	
+	private Gestore gestore ;
+	private SessioneDiVoto sessione; 
+	
     @FXML
     private ResourceBundle resources;
 
@@ -20,16 +31,35 @@ public class AvvioSessioneController extends Controller{
     @FXML
     private Button siBottone;
     
-    public void onNavigateFrom(Controller sender, Object parameter) {}
+
+    
+    public void onNavigateFrom(Controller sender, Object parameter) {
+    	
+    	try {
+	    	Object[] dati = (Object[]) parameter;
+	        gestore = (Gestore) dati[0];
+	        sessione = (SessioneDiVoto) dati[1];
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+    	
+    }
     
     @FXML
     void pressNoButton(ActionEvent event) {
-
+        Stage stage = (Stage) noBottone.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
     void pressSiButton(ActionEvent event) {
-
+    	sessione.setStatoSessione(StatoSessione.IN_CORSO);
+    	Stage stage = (Stage) noBottone.getScene().getWindow();
+        stage.close();
+        SessioneDiVotoDAOImpl sessioneDb = new SessioneDiVotoDAOImpl();
+        sessioneDb.updateSessioneDiVoto(sessione);
+        navigate("GestoreSessioniView", gestore);
     }
     
     @FXML
