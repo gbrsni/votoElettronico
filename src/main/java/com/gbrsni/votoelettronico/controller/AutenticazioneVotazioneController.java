feeb9b178ec80a0f835c12d7a91abc0f9c6bf7cb
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-03-30 22:07:45 +0200

    implementazione passaggio view per eseguire votazione sessione

--- src/main/java/com/gbrsni/votoelettronico/controller/AutenticazioneVotazioneController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/AutenticazioneVotazioneController.java
@@ -20,73 +20,90 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class AutenticazioneVotazioneController extends Controller{
-	
-	private Gestore gestore; 
+public class AutenticazioneVotazioneController extends Controller {
+
+	private Gestore gestore;
 	private SessioneDiVoto sessione;
-	
-    @FXML
-    private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private Button esciBottone;
-    
-    @FXML
-    private Label erroreLabel;
-    
-    @FXML
-    private PasswordField passwordTextField;
+	@FXML
+	private URL location;
 
-    @FXML
-    private TextField tesseraElettoraleTextField;
+	@FXML
+	private Button esciBottone;
 
-    @FXML
-    private Button votaBottone;
-    
-    
-    public void onNavigateFrom(Controller sender, Object parameter) {
-    	Object[] dati = (Object[]) parameter;
-        gestore = (Gestore) dati[0];
-        sessione = (SessioneDiVoto) dati[1];
-    }
-    
-    @FXML
-    void pressEsciBottone(ActionEvent event) {
+	@FXML
+	private Label erroreLabel;
 
-             Object[] parameter = new Object[] {gestore,sessione};           	
-             newStage("Uscita Votazione", "UscitaVotazioneView", parameter);
-    }
+	@FXML
+	private PasswordField passwordTextField;
 
-    @FXML
-    void pressVotaButton(ActionEvent event) {
-    	erroreLabel.setVisible(false);
-    	ElettoreDAOImpl elettoreDb = new ElettoreDAOImpl();
-    	Elettore elettore = elettoreDb.getElettoreByTesseraElettorale(tesseraElettoraleTextField.getText());
-    	if (elettore == null) { erroreLabel.setVisible(true);}
-	    	else {
-	    	SaltedPassword password = elettoreDb.getPasswordElettoreByUsername(elettore.getUsername());
-	    	if (password.checkPassword(passwordTextField.getText())) {
-	    		System.out.println("Accesso eseguito dall'elettore " + elettore.getUsername());
-	    		Object[] parameter = new Object[] {elettore, sessione, gestore};
-	    		navigate("VotazioneView", parameter);
-	    	
-	    	} else {	
-	    		erroreLabel.setVisible(true);
-	    	}
-    	}
-    }
+	@FXML
+	private TextField tesseraElettoraleTextField;
 
-    @FXML
-    void initialize() {
-    	assert erroreLabel != null : "fx:id=\"erroreLabel\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
-        assert esciBottone != null : "fx:id=\"esciBottone\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
-        assert passwordTextField != null : "fx:id=\"passwordTextField\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
-        assert tesseraElettoraleTextField != null : "fx:id=\"tesseraElettoraleTextField\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
-        assert votaBottone != null : "fx:id=\"votaBottone\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
-        erroreLabel.setText("Dati non corretti");
-    }
+	@FXML
+	private Button votaBottone;
+
+	public void onNavigateFrom(Controller sender, Object parameter) {
+		Object[] dati = (Object[]) parameter;
+		gestore = (Gestore) dati[0];
+		sessione = (SessioneDiVoto) dati[1];
+	}
+
+	@FXML
+	void pressEsciBottone(ActionEvent event) {
+
+		Object[] parameter = new Object[] { gestore, sessione };
+		newStage("Uscita Votazione", "UscitaVotazioneView", parameter);
+	}
+
+	@FXML
+	void pressVotaButton(ActionEvent event) {
+		erroreLabel.setVisible(false);
+		ElettoreDAOImpl elettoreDb = new ElettoreDAOImpl();
+		Elettore elettore = elettoreDb.getElettoreByTesseraElettorale(tesseraElettoraleTextField.getText());
+		if (elettore == null) {
+			erroreLabel.setVisible(true);
+		} else {
+			SaltedPassword password = elettoreDb.getPasswordElettoreByUsername(elettore.getUsername());
+			if (password.checkPassword(passwordTextField.getText())) {
+				System.out.println("Accesso eseguito dall'elettore " + elettore.getUsername());
+				Object[] parameter = new Object[] { elettore, sessione, gestore };
+				switch (sessione.getModVoto()) {
+				case ORDINALE:
+					// navigate("VotazioneOrdinaleView", parameter);
+					break;
+				case CATEGORICO:
+					navigate("VotazioneCategoricoView", parameter);
+					break;
+				case CATEGORICO_CON_PREFERENZE:
+					navigate("VotazioneCategoricoView", parameter);
+					break;
+				case REFERENDUM:
+					navigate("VotazioneReferendumView", parameter);
+					break;
+				}
+			} else {
+				erroreLabel.setVisible(true);
+			}
+		}
+	}
+
+	@FXML
+	void initialize() {
+		assert erroreLabel != null
+				: "fx:id=\"erroreLabel\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
+		assert esciBottone != null
+				: "fx:id=\"esciBottone\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
+		assert passwordTextField != null
+				: "fx:id=\"passwordTextField\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
+		assert tesseraElettoraleTextField != null
+				: "fx:id=\"tesseraElettoraleTextField\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
+		assert votaBottone != null
+				: "fx:id=\"votaBottone\" was not injected: check your FXML file 'AutenticazioneVotazioneView.fxml'.";
+		erroreLabel.setText("Dati non corretti");
+	}
 
 }
--- src/main/java/com/gbrsni/votoelettronico/controller/ElettoreSessioniController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/ElettoreSessioniController.java
@@ -20,90 +20,105 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
-public class ElettoreSessioniController extends Controller{
-	
+public class ElettoreSessioniController extends Controller {
+
 	private Elettore elettore;
 	private List<SessioneDiVoto> sessioni;
-	
-    @FXML
-    private ResourceBundle resources;
 
-    @FXML
-    private URL location;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private Button logoutBottone;
+	@FXML
+	private URL location;
 
-    @FXML
-    private Label nomeElettore;
+	@FXML
+	private Button logoutBottone;
 
-    @FXML
-    private VBox sessioniVbox;
-    
-    @Override
+	@FXML
+	private Label nomeElettore;
+
+	@FXML
+	private VBox sessioniVbox;
+
+	@Override
 	public void onNavigateFrom(Controller sender, Object parameter) {
 		elettore = (Elettore) parameter;
 		nomeElettore.setText(elettore.getNome());
 	}
-    
-    @FXML
-    void pressLogoutButton(ActionEvent event) {
-    	elettore = null;
-    	navigate("LoginController");
-    }
-    
-    //Visualizza Risultati sessione Scrutinata
-    private EventHandler<ActionEvent> votazione = new EventHandler<ActionEvent>() {
-        public void handle(ActionEvent e)
-        {
-        	SessioneDiVoto sessione = (SessioneDiVoto)((Button)e.getSource()).getUserData();
-            System.out.println("Id Sessione di Voto:" + sessione.getId());
-            Object[] parameter = new Object[] {elettore,sessione};
-            navigate("VotazioneView", parameter);
-        }
-    };
-    
-    private void init() {
-    	for (int i = 0; i < sessioni.size(); i++) {
-        	VBox sessioniVboxInterna = new VBox();
-        	HBox sessioniHboxNome = new HBox();
-        	HBox sessioniHboxDati = new HBox();
-        	Label sessioniLabelNome = new Label();
-        	Label sessioniLabelDati = new Label();
-        	sessioniLabelNome.setFont(new Font(25));
-        	sessioniLabelNome.setText(sessioni.get(i).getNome());
-        	sessioniLabelDati.setFont(new Font(15));
-        	sessioniLabelDati.setText(sessioni.get(i).getDescrizione() +  " Data: "+ sessioni.get(i).getData() + " modVoto: " + sessioni.get(i).getModVoto());	
-        	Region region1 = new Region();
-        	sessioniHboxNome.setHgrow(region1, Priority.ALWAYS);
-        	Button votoBottone = new Button("Vota");
-        	votoBottone.setUserData(sessioni.get(i));
-        	votoBottone.setOnAction(votazione);
-        	
-        	sessioniHboxNome.getChildren().add(sessioniLabelNome);
-        	sessioniHboxNome.getChildren().add(region1);
-        	sessioniHboxNome.getChildren().add(votoBottone);
-        	sessioniHboxDati.getChildren().add(sessioniLabelDati);
-        	sessioniVboxInterna.getChildren().add(sessioniHboxNome);
-        	sessioniVboxInterna.getChildren().add(sessioniHboxDati);
-        	sessioniVbox.getChildren().add(sessioniVboxInterna);
-    	}
-    	
-    	
-        //ORA BISOGNA INSERIRE IL CONTROLLO PER FAR COMPARIRE IL TASTO PER VOTARE SOLO SE L'ELETTORE NON HA GIà VOTATO
 
-    }
-    
-    @FXML
-    void initialize() {
-        assert logoutBottone != null : "fx:id=\"logoutBottone\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
-        assert nomeElettore != null : "fx:id=\"nomeElettore\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
-        assert sessioniVbox != null : "fx:id=\"sessioniVbox\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
-        SessioneDiVotoDAOImpl sessioniDb = new SessioneDiVotoDAOImpl();
-        sessioni = sessioniDb.getAllSessioneDiVotoByStato(StatoSessione.IN_CORSO);
-        init();
-    }
+	@FXML
+	void pressLogoutButton(ActionEvent event) {
+		elettore = null;
+		navigate("LoginView");
+	}
 
-	
+	// Visualizza Risultati sessione Scrutinata
+	private EventHandler<ActionEvent> votazione = new EventHandler<ActionEvent>() {
+		public void handle(ActionEvent e) {
+			SessioneDiVoto sessione = (SessioneDiVoto) ((Button) e.getSource()).getUserData();
+			System.out.println("Id Sessione di Voto:" + sessione.getId());
+			Object[] parameter = new Object[] { elettore, sessione };
+			switch (sessione.getModVoto()) {
+				case ORDINALE:
+					//navigate("VotazioneOrdinaleView", parameter);
+					break;
+				case CATEGORICO:
+					navigate("VotazioneCategoricoView", parameter);
+					break;
+				case CATEGORICO_CON_PREFERENZE:
+					navigate("VotazioneCategoricoView", parameter);
+					break;
+				case REFERENDUM:
+					navigate("VotazioneReferendumView", parameter);
+				break;
+			}
+		}
+	};
+
+	private void init() {
+		sessioniVbox.setPrefWidth(850);
+		for (int i = 0; i < sessioni.size(); i++) {
+			VBox sessioniVboxInterna = new VBox();
+			HBox sessioniHboxNome = new HBox();
+			HBox sessioniHboxDati = new HBox();
+			Label sessioniLabelNome = new Label();
+			Label sessioniLabelDati = new Label();
+			sessioniLabelNome.setFont(new Font(25));
+			sessioniLabelNome.setText(sessioni.get(i).getNome());
+			sessioniLabelDati.setFont(new Font(15));
+			sessioniLabelDati.setText(sessioni.get(i).getDescrizione() + " Data: " + sessioni.get(i).getData()
+					+ " modVoto: " + sessioni.get(i).getModVoto());
+			Region region1 = new Region();
+			sessioniHboxNome.setHgrow(region1, Priority.ALWAYS);
+			Button votoBottone = new Button("Vota");
+			votoBottone.setUserData(sessioni.get(i));
+			votoBottone.setOnAction(votazione);
+
+			sessioniHboxNome.getChildren().add(sessioniLabelNome);
+			sessioniHboxNome.getChildren().add(region1);
+			sessioniHboxNome.getChildren().add(votoBottone);
+			sessioniHboxDati.getChildren().add(sessioniLabelDati);
+			sessioniVboxInterna.getChildren().add(sessioniHboxNome);
+			sessioniVboxInterna.getChildren().add(sessioniHboxDati);
+			sessioniVbox.getChildren().add(sessioniVboxInterna);
+		}
+
+		// ORA BISOGNA INSERIRE IL CONTROLLO PER FAR COMPARIRE IL TASTO PER VOTARE SOLO
+		// SE L'ELETTORE NON HA GIà VOTATO
+
+	}
+
+	@FXML
+	void initialize() {
+		assert logoutBottone != null
+				: "fx:id=\"logoutBottone\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
+		assert nomeElettore != null
+				: "fx:id=\"nomeElettore\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
+		assert sessioniVbox != null
+				: "fx:id=\"sessioniVbox\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
+		SessioneDiVotoDAOImpl sessioniDb = new SessioneDiVotoDAOImpl();
+		sessioni = sessioniDb.getAllSessioneDiVotoByStato(StatoSessione.IN_CORSO);
+		init();
+	}
 
 }
