9f33ccca17a20c65e5544731e93f8b07231d6de2
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-04-01 12:04:06 +0200

    aggiunta visualizzazione solo delle sessioni a cui l'elettore non ha ancora votato

--- src/main/java/com/gbrsni/votoelettronico/controller/ElettoreSessioniController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/ElettoreSessioniController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import com.gbrsni.votoelettronico.data_access.SessioneDiVotoDAOImpl;
+import com.gbrsni.votoelettronico.data_access.VotiEspressiDAOImpl;
 import com.gbrsni.votoelettronico.models.Elettore;
 import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 import com.gbrsni.votoelettronico.models.StatoSessione;
@@ -44,12 +45,14 @@ public class ElettoreSessioniController extends Controller {
 	public void onNavigateFrom(Controller sender, Object parameter) {
 		elettore = (Elettore) parameter;
 		nomeElettore.setText(elettore.getNome());
+		init();
 	}
 
 	@FXML
 	void pressLogoutButton(ActionEvent event) {
 		elettore = null;
 		navigate("LoginView");
+		
 	}
 
 	// Visualizza Risultati sessione Scrutinata
@@ -59,17 +62,17 @@ public class ElettoreSessioniController extends Controller {
 			System.out.println("Id Sessione di Voto:" + sessione.getId());
 			Object[] parameter = new Object[] { elettore, sessione };
 			switch (sessione.getModVoto()) {
-				case ORDINALE:
-					navigate("VotazioneOrdinaleView", parameter);
-					break;
-				case CATEGORICO:
-					navigate("VotazioneCategoricoView", parameter);
-					break;
-				case CATEGORICO_CON_PREFERENZE:
-					navigate("VotazioneCategoricoView", parameter);
-					break;
-				case REFERENDUM:
-					navigate("VotazioneReferendumView", parameter);
+			case ORDINALE:
+				navigate("VotazioneOrdinaleView", parameter);
+				break;
+			case CATEGORICO:
+				navigate("VotazioneCategoricoView", parameter);
+				break;
+			case CATEGORICO_CON_PREFERENZE:
+				navigate("VotazioneCategoricoView", parameter);
+				break;
+			case REFERENDUM:
+				navigate("VotazioneReferendumView", parameter);
 				break;
 			}
 		}
@@ -77,34 +80,38 @@ public class ElettoreSessioniController extends Controller {
 
 	private void init() {
 		sessioniVbox.setPrefWidth(850);
+		
+		VotiEspressiDAOImpl votiEspressiDb = new VotiEspressiDAOImpl();
+		
 		for (int i = 0; i < sessioni.size(); i++) {
-			VBox sessioniVboxInterna = new VBox();
-			HBox sessioniHboxNome = new HBox();
-			HBox sessioniHboxDati = new HBox();
-			Label sessioniLabelNome = new Label();
-			Label sessioniLabelDati = new Label();
-			sessioniLabelNome.setFont(new Font(25));
-			sessioniLabelNome.setText(sessioni.get(i).getNome());
-			sessioniLabelDati.setFont(new Font(15));
-			sessioniLabelDati.setText(sessioni.get(i).getDescrizione() + " Data: " + sessioni.get(i).getData()
-					+ " modVoto: " + sessioni.get(i).getModVoto());
-			Region region1 = new Region();
-			sessioniHboxNome.setHgrow(region1, Priority.ALWAYS);
-			Button votoBottone = new Button("Vota");
-			votoBottone.setUserData(sessioni.get(i));
-			votoBottone.setOnAction(votazione);
+			if (!votiEspressiDb.existsVotoEspresso(sessioni.get(i), elettore)) {
+				VBox sessioniVboxInterna = new VBox();
+				HBox sessioniHboxNome = new HBox();
+				HBox sessioniHboxDati = new HBox();
+				Label sessioniLabelNome = new Label();
+				Label sessioniLabelDati = new Label();
+				sessioniLabelNome.setFont(new Font(25));
+				sessioniLabelNome.setText(sessioni.get(i).getNome());
+				sessioniLabelDati.setFont(new Font(15));
+				sessioniLabelDati.setText(sessioni.get(i).getDescrizione() + " Data: " + sessioni.get(i).getData()
+						+ " modVoto: " + sessioni.get(i).getModVoto());
+				Region region1 = new Region();
+				sessioniHboxNome.setHgrow(region1, Priority.ALWAYS);
+				Button votoBottone = new Button("Vota");
+				votoBottone.setUserData(sessioni.get(i));
+				votoBottone.setOnAction(votazione);
 
-			sessioniHboxNome.getChildren().add(sessioniLabelNome);
-			sessioniHboxNome.getChildren().add(region1);
-			sessioniHboxNome.getChildren().add(votoBottone);
-			sessioniHboxDati.getChildren().add(sessioniLabelDati);
-			sessioniVboxInterna.getChildren().add(sessioniHboxNome);
-			sessioniVboxInterna.getChildren().add(sessioniHboxDati);
-			sessioniVbox.getChildren().add(sessioniVboxInterna);
+				sessioniHboxNome.getChildren().add(sessioniLabelNome);
+				sessioniHboxNome.getChildren().add(region1);
+				sessioniHboxNome.getChildren().add(votoBottone);
+				sessioniHboxDati.getChildren().add(sessioniLabelDati);
+				sessioniVboxInterna.getChildren().add(sessioniHboxNome);
+				sessioniVboxInterna.getChildren().add(sessioniHboxDati);
+				sessioniVbox.getChildren().add(sessioniVboxInterna);
+			}
 		}
 
-		// ORA BISOGNA INSERIRE IL CONTROLLO PER FAR COMPARIRE IL TASTO PER VOTARE SOLO
-		// SE L'ELETTORE NON HA GIà VOTATO
+	
 
 	}
 
@@ -118,7 +125,7 @@ public class ElettoreSessioniController extends Controller {
 				: "fx:id=\"sessioniVbox\" was not injected: check your FXML file 'ElettoreSessioniView.fxml'.";
 		SessioneDiVotoDAOImpl sessioniDb = new SessioneDiVotoDAOImpl();
 		sessioni = sessioniDb.getAllSessioneDiVotoByStato(StatoSessione.IN_CORSO);
-		init();
+		
 	}
 
 }
