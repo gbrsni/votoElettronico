1ca0f72334954c5728b323f6f410ce9260dc5972
alecuccoUnimi <alessandro.cucco1@studenti.unimi.it>
2022-04-01 12:02:20 +0200

    aggiunta metodo per ottenere dal db tutte le sessioni a cui un elettore ha votato

--- src/main/java/com/gbrsni/votoelettronico/data_access/VotiEspressiDAO.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/VotiEspressiDAO.java
@@ -1,8 +1,11 @@
 package com.gbrsni.votoelettronico.data_access;
 
+import java.util.List;
+
 import com.gbrsni.votoelettronico.models.Elettore;
 import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 
 public interface VotiEspressiDAO {
 	public boolean existsVotoEspresso(SessioneDiVoto sessioneDiVoto, Elettore elettore);
+	public List<SessioneDiVoto> allExistsVotoEspressoByElettore(Elettore elettore);
 }
--- src/main/java/com/gbrsni/votoelettronico/data_access/VotiEspressiDAOImpl.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/VotiEspressiDAOImpl.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.gbrsni.votoelettronico.models.Elettore;
@@ -11,7 +14,7 @@ import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 
 public class VotiEspressiDAOImpl implements VotiEspressiDAO {
 	private Connection connection = DBConnection.getConnection();
-
+	
 	// Attenzione! Restituisce false anche se viene sollevata un'eccezione durante l'esecuzione del metodo
 	@Override
 	public boolean existsVotoEspresso(SessioneDiVoto sessioneDiVoto, Elettore elettore) {
@@ -19,18 +22,42 @@ public class VotiEspressiDAOImpl implements VotiEspressiDAO {
 		Objects.requireNonNull(elettore);
 
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM votiespressi WHERE sessioni = ?, elettori = ?");
+			PreparedStatement ps = connection.prepareStatement("SELECT * FROM votiespressi WHERE sessioni = ? and  elettori = ?");
 			ps.setInt(1, sessioneDiVoto.getId());
 			ps.setString(2, elettore.getUsername());
 			ResultSet rs = ps.executeQuery();
 			
+			boolean risultato = rs.next();
 			ps.close();
 
-			return rs.next();
+			return risultato;
+			
 		} catch (SQLException e) {
 			System.out.println("Errore durante la ricerca del voto espresso da " + elettore.toString() + " per la sessione di voto " + sessioneDiVoto.toString());
 			e.printStackTrace();
 			return false;
 		}
+	}
+	
+	@Override
+	public List<SessioneDiVoto> allExistsVotoEspressoByElettore(Elettore elettore) {
+		Objects.requireNonNull(elettore);
+		List<SessioneDiVoto> s = new ArrayList<>();;
+		try {
+			PreparedStatement ps = connection.prepareStatement("SELECT * FROM votiespressi WHERE elettori = ?");
+			ps.setString(1, elettore.getUsername());
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				LocalDate data = LocalDate.of(rs.getDate("data").getYear(), rs.getDate("data").getMonth(), rs.getDate("data").getDate());
+				s.add(new SessioneDiVoto(rs.getInt("id"), rs.getString("nome"), rs.getString("descrizione") , data, rs.getString("modvoto"), rs.getString("modvittoria"), rs.getString("stato"), rs.getInt("nvoti")));
+			}
+			ps.close();
+
+		} catch (SQLException e) {
+			System.out.println("Errore durante selezione voti espressi per elettore " +  elettore.getUsername());
+			e.printStackTrace();
+		}
+		return s;
 	}
 }
