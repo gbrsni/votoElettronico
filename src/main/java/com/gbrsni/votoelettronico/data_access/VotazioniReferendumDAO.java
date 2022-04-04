f2577854298eef98dec05d70d54d61da3ee19c1b
gbrsni <giacomo.bersani@studenti.unimi.it>
2022-04-04 09:34:00 +0200

    Aggiunto metodo per ottenere tutte le votazioni di un referendum

--- src/main/java/com/gbrsni/votoelettronico/data_access/VotazioniReferendumDAO.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/VotazioniReferendumDAO.java
@@ -6,6 +6,6 @@ import com.gbrsni.votoelettronico.models.OpzioneReferendum;
 import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 
 public interface VotazioniReferendumDAO {
-	public List<OpzioneReferendum> getAllVotazioniReferendumBySessione(SessioneDiVoto sessione);
-	public void addVotazioniReferendumBySessione(SessioneDiVoto sessione, OpzioneReferendum opzione);
+	public List<OpzioneReferendum> getAllVotazioniReferendumBySessione(SessioneDiVoto sessioneDiVoto);
+	public void addVotazioniReferendumBySessione(SessioneDiVoto sessioneDiVoto, OpzioneReferendum opzione);
 }
