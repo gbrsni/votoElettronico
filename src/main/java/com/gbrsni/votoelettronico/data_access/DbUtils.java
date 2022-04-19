c813ba28c952920a4b073cf875b8d04769a89ef0
aless <aless@AleCuccoPc>
2022-04-19 20:15:47 +0200

    aggiunta chiusura connessione

--- src/main/java/com/gbrsni/votoelettronico/data_access/DbUtils.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/DbUtils.java
@@ -1,26 +1,35 @@
 package com.gbrsni.votoelettronico.data_access;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public  class DbUtils {
-	
-	/**chiuse il resultSet rs*/
-	public static void closeStatement(ResultSet rs) {
-		try { 
-			rs.close(); 
-		} catch (Exception e) { 
+public class DbUtils {
+
+	/** chiuse il resultSet rs */
+	public static void closeStatement(PreparedStatement ps) {
+		try {
+			ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/** chiude il prepareStatement ps */
+	public static void closeResultSet(ResultSet rs) {
+		try {
+			rs.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	/**chiude il prepareStatement p */
-	public static void closeResultSet(PreparedStatement ps) {
-	try {
-        ps.close();
-    } catch (SQLException e) { 
-    	e.printStackTrace();
-    }
+	public static void closeConnection(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
