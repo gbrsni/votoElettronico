6bb0769d6d93448f1e0fb46cdc8913e07e4eb32d
aless <aless@AleCuccoPc>
2022-04-20 20:50:26 +0200

    BugFix chiusura Statement e ResultSet

--- src/main/java/com/gbrsni/votoelettronico/data_access/ElettoreDAOImpl.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/ElettoreDAOImpl.java
@@ -13,82 +13,89 @@ public class ElettoreDAOImpl implements ElettoreDAO {
 
 	public ElettoreDAOImpl() {
 	}
-
+	
+	/**restituisce tutti gli elettori*/
 	@Override
 	public List<Elettore> getAllElettore() {
 		List<Elettore> res = new ArrayList<>();
-
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM elettori");
-			ResultSet rs = ps.executeQuery();
+			ps = connection.prepareStatement("SELECT * FROM elettori");
+			rs = ps.executeQuery();
 			
 			while (rs.next()) {
 				res.add(new Elettore(rs.getString("username"), rs.getString("nome"), rs.getString("cognome"), rs.getString("codicefiscale"), rs.getString("tesseraelettorale")));
 			}
 			
-			ps.close();
 		} catch (SQLException e) {
 			System.out.println("Errore durante l'ottenimento di tutti gli elettori");
 			e.printStackTrace();
 		}
 		
+		finally { DbUtils.closeResultSet(rs); DbUtils.closeStatement(ps); }
 		return res;
 	}
 
+	/**Aggiorna elettore e*/
 	@Override
 	public void updateElettore(Elettore e) {
 		Objects.requireNonNull(e);
+		PreparedStatement ps = null;
+		
 		try {
-			PreparedStatement ps = connection.prepareStatement("UPDATE elettori SET nome = ?, cognome = ?, codicefiscale = ?, tesseraelettorale = ? WHERE username = ?");
+			ps = connection.prepareStatement("UPDATE elettori SET nome = ?, cognome = ?, codicefiscale = ?, tesseraelettorale = ? WHERE username = ?");
 			ps.setString(1, e.getNome());
 			ps.setString(2, e.getCognome());
 			ps.setString(3, e.getTesseraElettorale());
 			ps.setString(4, e.getCodiceFiscale());
 			ps.setString(5, e.getUsername());
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Aggiornato elettore " + e.toString() + " nel database");
 		} catch (SQLException ex) {
 			System.out.println("Errore durante l'aggiornamento dell'elettore " + e.toString());
 			ex.printStackTrace();
-			return;
 		}
-		System.out.println("Aggiornato elettore " + e.toString() + " nel database");
+		finally { DbUtils.closeStatement(ps); }
 	}
-
+	
+	/**elimina elettore e*/
 	@Override
 	public void deleteElettore(Elettore e) {
 		Objects.requireNonNull(e);
+		PreparedStatement ps = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("DELETE FROM elettori WHERE username = ?");
+			ps = connection.prepareStatement("DELETE FROM elettori WHERE username = ?");
 			ps.setString(1, e.getUsername());
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Rimosso elettore " + e.toString() + " dal database");
 		} catch (SQLException ex) {
 			System.out.println("Errore durante la rimozione dell'elettore " + e.toString());
 			ex.printStackTrace();
-			return;
 		}
-		System.out.println("Rimosso elettore " + e.toString() + " dal database");
+		finally { DbUtils.closeStatement(ps); }
 	}
-
+	
+	/**aggiunti elettore e */
 	@Override
 	public void addElettore(Elettore e) {
 		Objects.requireNonNull(e);
+		PreparedStatement ps = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("INSERT INTO elettori (username, nome, cognome, codicefiscale, tesseraelettorale) VALUES (?, ?, ?, ?, ?)");
+			ps = connection.prepareStatement("INSERT INTO elettori (username, nome, cognome, codicefiscale, tesseraelettorale) VALUES (?, ?, ?, ?, ?)");
 			ps.setString(1, e.getUsername());
 			ps.setString(2, e.getNome());
 			ps.setString(3, e.getCognome());
 			ps.setString(4, e.getTesseraElettorale());
 			ps.setString(5, e.getCodiceFiscale());
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Inserito elettore " + e.toString() + " dal database");
 		} catch (SQLException ex) {
 			System.out.println("Errore durante l'inserimento dell'elettore " + e.toString());
 			ex.printStackTrace();
-			return;
 		}
-		System.out.println("Inserito elettore " + e.toString() + " dal database");
+		finally { DbUtils.closeStatement(ps); }
 	}
 
 	@Override
@@ -102,80 +109,89 @@ public class ElettoreDAOImpl implements ElettoreDAO {
 		addElettore(e);
 	}
 
+	/**restituisce l'elettore dato l'username */
 	@Override
 	public Elettore getElettoreByUsername(String username) {
 		Objects.requireNonNull(username);
 		Elettore e = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM elettori WHERE username = ?");
+			ps = connection.prepareStatement("SELECT * FROM elettori WHERE username = ?");
 			ps.setString(1, username);
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			
-			
-			if (rs.next() != false) {
+			if (rs.next())
 				e = new Elettore(rs.getString("username"), rs.getString("nome"), rs.getString("cognome"), rs.getString("codicefiscale"), rs.getString("tesseraelettorale"));
-			}
-			ps.close();
+			System.out.println("Ottenuto elettore con username " + username);
 		} catch (SQLException ex) {
 			System.out.println("Errore durante l'ottenimento dell'elettore con username" + username);
 			ex.printStackTrace();
-			return null;
 		}
-		System.out.println("Ottenuto elettore con username " + username);
+		finally { DbUtils.closeResultSet(rs); DbUtils.closeStatement(ps); }
 		return e;
 	}
 	
+	/**Restituisce l'elettore data la tessera Elettorale*/
 	@Override
 	public Elettore getElettoreByTesseraElettorale(String tesseraElettorale) {
 		Objects.requireNonNull(tesseraElettorale);
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		Elettore e = null;
+		
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM elettori WHERE tesseraelettorale = ?");
+			ps = connection.prepareStatement("SELECT * FROM elettori WHERE tesseraelettorale = ?");
 			ps.setString(1, tesseraElettorale);
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			
-			
-			if (rs.next() != false) {
+			if (rs.next()) 
 				e = new Elettore(rs.getString("username"), rs.getString("nome"), rs.getString("cognome"), rs.getString("codicefiscale"), rs.getString("tesseraelettorale"));
-			}
 			
-			ps.close();
+			System.out.println("Ottenuto elettore con tessera elettorale " + tesseraElettorale);
+			
 		} catch (SQLException ex) {
 			System.out.println("Errore durante l'ottenimento dell'elettore con tessera elettorale" + tesseraElettorale);
 			ex.printStackTrace();
-			return null;
 		}
-		System.out.println("Ottenuto elettore con tessera elettorale " + tesseraElettorale);
+		
+		finally { DbUtils.closeResultSet(rs); DbUtils.closeStatement(ps); }
 		return e;
 	}
 
-
+	/**resistuisce la SaltedPassword dato l'username dell'elettore*/
 	@Override
 	public SaltedPassword getPasswordElettoreByUsername(String username) {
 		Objects.requireNonNull(username);
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		SaltedPassword sp = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM passwordelettori WHERE elettori = ?");
+			ps = connection.prepareStatement("SELECT * FROM passwordelettori WHERE elettori = ?");
 			ps.setString(1, username);
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			
-			if (rs.next() != false) {
+			if (rs.next()) 
 				sp = new SaltedPassword(rs.getString("hash"), rs.getString("salt"));
-			}
-			ps.close();
+			
+			System.out.println("Ottenuta password dell'elettore con username " + username);
 		} catch (SQLException ex) {
 			System.out.println("Errore durante l'ottenimento della password dell'elettore con username" + username);
 			ex.printStackTrace();
-			return null;
 		}
-		System.out.println("Ottenuta password dell'elettore con username " + username);
+		
+		finally { DbUtils.closeResultSet(rs); DbUtils.closeStatement(ps); }
 		return sp;
 	}
-
+	
+	/**imposta SaltedPassword per l'elettore con username indicato */
 	@Override
 	public void setPasswordElettoreByUsername(String username, SaltedPassword sp) {
 		Objects.requireNonNull(username);
 		Objects.requireNonNull(sp);
+		PreparedStatement ps = null;
+		
 		try {
 			String query;
 			if (getPasswordElettoreByUsername(username) == null) {
@@ -183,17 +199,16 @@ public class ElettoreDAOImpl implements ElettoreDAO {
 			} else {
 				query = "UPDATE passwordelettori SET salt = ?, hash = ? WHERE elettori = ?";
 			}
-			PreparedStatement ps = connection.prepareStatement(query);
+			ps = connection.prepareStatement(query);
 			ps.setString(1, sp.getSalt());
 			ps.setString(2, sp.getHash());
 			ps.setString(3, username);
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Modificata password dell'elettore con username " + username);
 		} catch (SQLException ex) {
 			System.out.println("Errore durante la modifica della password dell'elettore con username " + username);
 			ex.printStackTrace();
-			return;
 		}
-		System.out.println("Modificata password dell'elettore con username " + username);
+		finally { DbUtils.closeStatement(ps); }
 	}
 }
