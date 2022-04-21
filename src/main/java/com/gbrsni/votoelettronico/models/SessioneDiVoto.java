151aedb6265947bdfc679645692fda3bbf84c772
aless <aless@AleCuccoPc>
2022-04-21 15:58:46 +0200

    modifica gerarchia

--- src/main/java/com/gbrsni/votoelettronico/models/SessioneDiVoto.java
+++ src/main/java/com/gbrsni/votoelettronico/models/SessioneDiVoto.java
@@ -14,7 +14,8 @@ import com.gbrsni.votoelettronico.data_access.VotiEspressiDAO;
 import com.gbrsni.votoelettronico.data_access.VotiEspressiDAOImpl;
 import com.gbrsni.votoelettronico.data_access.VotiPartitiDAOImpl;
 
-public class SessioneDiVoto {
+public abstract class SessioneDiVoto {
+	
 	private int id;
 	private String nome;
 	private String descrizione;
@@ -23,7 +24,6 @@ public class SessioneDiVoto {
 	private ModVittoria modVittoria;
 	private StatoSessione statoSessione;
 	private int nvoti;
-	
 	
 	public SessioneDiVoto(int id, String nome, String descrizione, LocalDate data, ModVoto modVoto, ModVittoria modVittoria, StatoSessione statoSessione, int nvoti) {
 		Objects.requireNonNull(nome);
@@ -47,7 +47,6 @@ public class SessioneDiVoto {
 		}
 	}
 
-	
 	public SessioneDiVoto(int id, String nome, String descrizione, LocalDate data,String modVoto, String modVittoria, String statoSessione, int nvoti) throws IllegalArgumentException {
 		Objects.requireNonNull(nome);
 		Objects.requireNonNull(data);
@@ -145,70 +144,4 @@ public class SessioneDiVoto {
 		this.id = id;
 	}
 	
-	public Map<Candidato, Integer> getVotazioniCandidati() {
-		VotazioniCandidatiDAO votazioniCandidatiDAO = new VotazioniCandidatiDAOImpl();
-		return votazioniCandidatiDAO.getVotazioniCandidatiBySessione(this);
-	}
-	
-	public void setVotazioniCandidato(Candidato candidato, int voti) {
-		Objects.requireNonNull(candidato);
-		
-		VotazioniCandidatiDAO votazioniCandidatiDAO = new VotazioniCandidatiDAOImpl();
-		votazioniCandidatiDAO.setVotazioniCandidatiBySessione(this, candidato, voti);
-	}
-	
-	public Map<Partito, Integer> getVotazioniPartiti() {
-		VotazioniPartitiDAO votazioniPartitiDAO = new VotazioniPartitiDAOImpl();
-		return votazioniPartitiDAO.getVotazioniPartitiBySessione(this);
-	}
-	
-	public void setVotazioniPartito(Partito partito, int voti) {
-		Objects.requireNonNull(partito);
-		
-		VotazioniPartitiDAO votazioniPartitiDAO = new VotazioniPartitiDAOImpl();
-		votazioniPartitiDAO.setVotazioniPartitiBySessione(this, partito, voti);
-	}
-	
-	public boolean elettoreHaPartecipato(Elettore elettore) {
-		Objects.requireNonNull(elettore);
-
-		VotiEspressiDAO votiEspressiDAO = new VotiEspressiDAOImpl();
-		return votiEspressiDAO.existsVotoEspresso(this, elettore);
-	}
-	
-	public Map<Candidato, Integer> getVotiCandidati() {
-		VotiCandidatiDAOImpl votiCandidatiDAO = new VotiCandidatiDAOImpl();
-		return votiCandidatiDAO.getVotiCandidatiBySessione(this);
-	}
-	
-	public void setVotiCandidati(Candidato candidato) {
-		VotiCandidatiDAOImpl votiCandidatiDAO = new VotiCandidatiDAOImpl();
-		votiCandidatiDAO.addVotiCandidatiBySessione(this, candidato);
-	}
-	
-	public Map<Partito,Integer> getVotiPartiti() {
-		VotiPartitiDAOImpl votiPartitiDAO = new VotiPartitiDAOImpl();
-		return votiPartitiDAO.getVotiPartitiBySessione(this);
-	}
-	
-	public void setVotiPartiti(Partito partito) {
-		VotiPartitiDAOImpl votiPartitiDAO = new VotiPartitiDAOImpl();
-		votiPartitiDAO.addVotiPartitiBySessione(this, partito);
-	}
-	
-	public Integer getVotiAstenuti() {
-		VotiAstenutiDAOImpl votiAstenutiDAO = new VotiAstenutiDAOImpl();
-		return votiAstenutiDAO.getVotiAstenutiBySessione(this);
-	}
-	
-	public void setVotiAstenuti() {
-		VotiAstenutiDAOImpl votiAstenutiDAO = new VotiAstenutiDAOImpl();
-		votiAstenutiDAO.addVotiAstenutiBySessione(this);
-	}
-	
-	public void updateVotiCandidati(Candidato candidato, Integer nvoti) {}
-	public void updateVotiPartiti(Partito partito, Integer nvoti) {}
-	public void updateVotiAstenuti(Integer nvoti) {}
-
-	// TODO: toString()
 }
