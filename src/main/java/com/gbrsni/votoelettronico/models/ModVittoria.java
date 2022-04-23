9aa0ea398f7ee2740db41a257b66957386f9bda1
aless <aless@AleCuccoPc>
2022-04-23 20:19:20 +0200

    Override metodo toString

--- src/main/java/com/gbrsni/votoelettronico/models/ModVittoria.java
+++ src/main/java/com/gbrsni/votoelettronico/models/ModVittoria.java
@@ -4,5 +4,16 @@ public enum ModVittoria {
 	MAGGIORANZA,
 	MAGGIORANZA_ASSOLUTA,
 	REFERENDUM_SENZA_QUORUM,
-	REFERENDUM_CON_QUORUM
+	REFERENDUM_CON_QUORUM;
+	
+	@Override
+	  public String toString() {
+	    switch(this) {
+	      case MAGGIORANZA: return "MAGGIORANZA";
+	      case MAGGIORANZA_ASSOLUTA: return "MAGGIORANZA_ASSOLUTA";
+	      case REFERENDUM_SENZA_QUORUM: return "REFERENDUM_SENZA_QUORUM";
+	      case REFERENDUM_CON_QUORUM: return "REFERENDUM_CON_QUORUM";
+	      default: throw new IllegalArgumentException();
+	    }
+	  }
 }
