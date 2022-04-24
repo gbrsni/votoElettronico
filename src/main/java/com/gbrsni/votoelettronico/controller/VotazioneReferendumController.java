377bd9fd51a49c2b15baa1da976ef726c0b2db48
aless <aless@AleCuccoPc>
2022-04-24 14:32:18 +0200

    creazione interfaccia per raccolta voto Referendum

--- src/main/java/com/gbrsni/votoelettronico/controller/VotazioneReferendumController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/VotazioneReferendumController.java
@@ -10,8 +10,8 @@ import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 
 public class VotazioneReferendumController extends Controller {
 	
@@ -19,17 +19,20 @@ public class VotazioneReferendumController extends Controller {
 	private Gestore gestore; 
 	private SessioneDiVoto sessione; 
 	
-    @FXML
+	@FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
 
     @FXML
-    private CheckBox contrarioCheckBox;
+    private Button annullaSelezioneButton;
 
     @FXML
-    private CheckBox favorevoleCheckBox;
+    private Label dataLabel;
+
+    @FXML
+    private Label modVittoriaLabel;
 
     @FXML
     private Label modVotoLabel;
@@ -41,7 +44,11 @@ public class VotazioneReferendumController extends Controller {
     private Label nomeLabel;
 
     @FXML
-    private Button votaBottone;
+    private ToggleGroup radioGroup;
+
+    @FXML
+    private Button votaButton;
+
     
     @Override
 	public void onNavigateFrom(Controller sender, Object parameter) {
@@ -54,24 +61,35 @@ public class VotazioneReferendumController extends Controller {
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		nomeElettore.setText(elettore.getNome());
+		nomeLabel.setText("Sessione:" + sessione.getNome());
+		modVotoLabel.setText("Mod. Voto:" + sessione.getModVoto());
+		modVittoriaLabel.setText("Mod. Vittoria:" + sessione.getModVittoria());
+		dataLabel.setText("Data: " + sessione.getData());
 	}
     
     @FXML
+    void pressAnnullaSelezioneButton(ActionEvent event) {
+    	radioGroup.getSelectedToggle().setSelected(false);
+    }
+    
+    @FXML
     void pressVotaButton(ActionEvent event) {
-
+    	System.out.println(radioGroup.getSelectedToggle());
+    	//GESTIRE PASSAGGIO VOTO
     }
 
     @FXML
     void initialize() {
-        assert contrarioCheckBox != null : "fx:id=\"contrarioCheckBox\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
-        assert favorevoleCheckBox != null : "fx:id=\"favorevoleCheckBox\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
+    	assert annullaSelezioneButton != null : "fx:id=\"annullaSelezioneButton\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
+        assert dataLabel != null : "fx:id=\"dataLabel\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
+        assert modVittoriaLabel != null : "fx:id=\"modVittoriaLabel\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
         assert modVotoLabel != null : "fx:id=\"modVotoLabel\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
         assert nomeElettore != null : "fx:id=\"nomeElettore\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
         assert nomeLabel != null : "fx:id=\"nomeLabel\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
-        assert votaBottone != null : "fx:id=\"votaBottone\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
+        assert radioGroup != null : "fx:id=\"radioGroup\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
+        assert votaButton != null : "fx:id=\"votaButton\" was not injected: check your FXML file 'VotazioneReferendumView.fxml'.";
         
     }
-
-	
 
 }
