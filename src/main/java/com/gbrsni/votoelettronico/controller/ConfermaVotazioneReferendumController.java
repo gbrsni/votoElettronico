a776f36bfc195612aba131fdd1156bb5e8fefd4c
aless <aless@AleCuccoPc>
2022-04-25 20:50:19 +0200

    aggiunta view e controller per conferma voto referendum

---
+++ src/main/java/com/gbrsni/votoelettronico/controller/ConfermaVotazioneReferendumController.java
@@ -0,0 +1,52 @@
+package com.gbrsni.votoelettronico.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ConfermaVotazioneReferendumController extends Controller{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button annullaButton;
+
+    @FXML
+    private Button confermaButton;
+
+    @FXML
+    private VBox votoVbox;
+    
+    
+    @Override
+	public void onNavigateFrom(Controller sender, Object parameter) {
+
+	}
+    
+    @FXML
+    void pressAnnullaButton(ActionEvent event) {
+    	Stage stage = (Stage) annullaButton.getScene().getWindow();
+		closeStage(stage);
+    }
+
+    @FXML
+    void pressConfermaButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert annullaButton != null : "fx:id=\"annullaButton\" was not injected: check your FXML file 'ConfermaVotazioneReferendumView.fxml'.";
+        assert confermaButton != null : "fx:id=\"confermaButton\" was not injected: check your FXML file 'ConfermaVotazioneReferendumView.fxml'.";
+        assert votoVbox != null : "fx:id=\"votoVbox\" was not injected: check your FXML file 'ConfermaVotazioneReferendumView.fxml'.";
+    }
+
+}
