cd21cb54025e8af8eddb9091a0c763acba7670b4
aless <aless@AleCuccoPc>
2022-04-26 22:18:54 +0200

    impostazione per salvataggio voto elettore

--- src/main/java/com/gbrsni/votoelettronico/controller/ConfermaVotazioneReferendumController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/ConfermaVotazioneReferendumController.java
@@ -2,14 +2,27 @@ package com.gbrsni.votoelettronico.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.gbrsni.votoelettronico.models.Elettore;
+import com.gbrsni.votoelettronico.models.Gestore;
+import com.gbrsni.votoelettronico.models.SessioneDiVoto;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ConfermaVotazioneReferendumController extends Controller{
-
+	
+	private Gestore gestore;
+	private Elettore elettore;
+	private SessioneDiVoto sessione;
+	Boolean scelta; 
+	
     @FXML
     private ResourceBundle resources;
 
@@ -28,8 +41,26 @@ public class ConfermaVotazioneReferendumController extends Controller{
     
     @Override
 	public void onNavigateFrom(Controller sender, Object parameter) {
-
+    	Object[] data = (Object[]) parameter;
+		elettore = (Elettore) data[0];
+		sessione = (SessioneDiVoto) data[1];
+		gestore = (Gestore) data[2];	
+		scelta = (Boolean) data[3];
+		init();
 	}
+    
+    private void init() {
+    	Label voto = new Label();
+    	voto.setFont(new Font(34));
+    	votoVbox.setAlignment(Pos.CENTER_LEFT);
+    	if(scelta == null)
+    		voto.setText("Scheda Bianca");
+    	else if(scelta)
+    		voto.setText("Favorevole");
+    	else
+    		voto.setText("Contrario");
+    	votoVbox.getChildren().add(voto);
+    }
     
     @FXML
     void pressAnnullaButton(ActionEvent event) {
