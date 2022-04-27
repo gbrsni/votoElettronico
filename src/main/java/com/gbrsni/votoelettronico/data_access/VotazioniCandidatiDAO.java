e75e4dfdf5c1eccf50699410027ca1a910116865
gbrsni <giacomo.bersani@studenti.unimi.it>
2022-04-27 15:12:28 +0200

    importazioni

--- src/main/java/com/gbrsni/votoelettronico/data_access/VotazioniCandidatiDAO.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/VotazioniCandidatiDAO.java
@@ -1,10 +1,13 @@
 package com.gbrsni.votoelettronico.data_access;
 
 
+import java.util.List;
 import java.util.Map;
 
 import com.gbrsni.votoelettronico.models.Candidato;
 import com.gbrsni.votoelettronico.models.SessioneDiVoto;
+
+import javafx.util.Pair;
 
 public interface VotazioniCandidatiDAO {
 	public List<Pair<Candidato, Integer>> getVotazioniCandidatiBySessione(SessioneDiVoto sessioneDiVoto);
