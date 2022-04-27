a9b95de9ccce775807ab85ccaad5e937dd764042
gbrsni <giacomo.bersani@studenti.unimi.it>
2022-04-27 15:25:13 +0200

    nuovo costruttore

--- src/main/java/com/gbrsni/votoelettronico/models/SessionStateException.java
+++ src/main/java/com/gbrsni/votoelettronico/models/SessionStateException.java
@@ -4,4 +4,8 @@ public class SessionStateException extends RuntimeException {
 	public SessionStateException(String str) {
 		super(str);
 	}
+	
+	public SessionStateException(StatoSessione expected, StatoSessione found, int sessionId) {
+		super("La sessione con id " + sessionId + " era in stato " + found + " invece che in stato " + expected);
+	}
 }
