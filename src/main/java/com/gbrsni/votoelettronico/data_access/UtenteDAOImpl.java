6b03358630de23713864a4ba40e25cc5c58d5cc0
aless <aless@AleCuccoPc>
2022-04-28 19:17:51 +0200

    aggiunta commenti, chiusura statement e resulstSet

--- src/main/java/com/gbrsni/votoelettronico/data_access/UtenteDAOImpl.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/UtenteDAOImpl.java
@@ -5,15 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 import com.gbrsni.votoelettronico.models.Utente;
 
 public class UtenteDAOImpl implements UtenteDAO {
 	private Connection connection = DBConnection.getConnection();
 	
-	public UtenteDAOImpl() {
-	}
+	public UtenteDAOImpl() {}
 	
+	/**resituisce tutti gli utenti */
 	@Override
 	public List<Utente> getAllUtente() {
 		List<Utente> res = new ArrayList<>();
@@ -26,39 +25,41 @@ public class UtenteDAOImpl implements UtenteDAO {
 		
 		return res;
 	}
-
+	
+	/**aggiorna l'utente u*/
 	@Override
 	public void updateUtente(Utente u) {
 		Objects.requireNonNull(u);
+		PreparedStatement ps = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("UPDATE utenti SET nome = ?, cognome = ? WHERE username = ?");
+			ps = connection.prepareStatement("UPDATE utenti SET nome = ?, cognome = ? WHERE username = ?");
 			ps.setString(1, u.getNome());
 			ps.setString(2, u.getCognome());
 			ps.setString(3, u.getUsername());
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Aggiornato utente " + u.toString() + " nel database");
 		} catch (SQLException e) {
 			System.out.println("Errore durante l'aggiornamento dell'utente " + u.toString());
 			e.printStackTrace();
-			return;
 		}
-		System.out.println("Aggiornato utente " + u.toString() + " nel database");
+		finally { DbUtils.closeStatement(ps); }
 	}
-
+	
+	/**elimina l'utente u dal database*/
 	@Override
 	public void deleteUtente(Utente u) {
 		Objects.requireNonNull(u);
+		PreparedStatement ps = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("DELETE FROM utenti  WHERE username = ?");
+			ps = connection.prepareStatement("DELETE FROM utenti  WHERE username = ?");
 			ps.setString(1, u.getUsername());
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Utente " + u.toString() + " rimosso dal database");
 		} catch (SQLException e) {
 			System.out.println("Errore durante la rimozione dell'utente " + u.toString());
 			e.printStackTrace();
-			return;
 		}
-		System.out.println("Utente " + u.toString() + " rimosso dal database");
+		finally {  DbUtils.closeStatement(ps); }
 	}
 
 }
