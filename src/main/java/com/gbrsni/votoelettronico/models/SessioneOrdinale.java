eb42259a611517f1c42c456c9c1d8aac295b93d0
aless <aless@AleCuccoPc>
2022-04-28 18:18:59 +0200

    eliminazione import di package non usati

--- src/main/java/com/gbrsni/votoelettronico/models/SessioneOrdinale.java
+++ src/main/java/com/gbrsni/votoelettronico/models/SessioneOrdinale.java
@@ -1,18 +1,7 @@
 package com.gbrsni.votoelettronico.models;
 
 import java.time.LocalDate;
-import java.util.Map;
-import java.util.Objects;
 
-import com.gbrsni.votoelettronico.data_access.VotazioniCandidatiDAO;
-import com.gbrsni.votoelettronico.data_access.VotazioniCandidatiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotazioniPartitiDAO;
-import com.gbrsni.votoelettronico.data_access.VotazioniPartitiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiAstenutiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiCandidatiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiEspressiDAO;
-import com.gbrsni.votoelettronico.data_access.VotiEspressiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiPartitiDAOImpl;
 
 public class SessioneOrdinale extends SessioneDiVoto{
 	
