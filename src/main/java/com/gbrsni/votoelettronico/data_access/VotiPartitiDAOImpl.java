1b9dd76628a64207c26a71852329531aecb16f30
aless <aless@AleCuccoPc>
2022-04-29 21:18:57 +0200

    aggiunta chiusura Statement e ResultSet

--- src/main/java/com/gbrsni/votoelettronico/data_access/VotiPartitiDAOImpl.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/VotiPartitiDAOImpl.java
@@ -17,68 +17,61 @@ public class VotiPartitiDAOImpl implements VotiPartitiDAO{
 	
 	private Connection connection = DBConnection.getConnection();
 	
-	//get partiti della sessione di voto
+	/**ottiene tutti i partiti per la sessione di voto*/
 	@Override
 	public Map<Partito, Integer> getVotiPartitiBySessione(SessioneDiVoto sessione) {
 		Objects.requireNonNull(sessione);
-		
 		Map<Partito, Integer> res = new HashMap<>();
-
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM partiti INNER JOIN votipartiti ON partiti.id = votipartiti.partiti WHERE votipartiti.sessioni = ?");
+			ps = connection.prepareStatement("SELECT * FROM partiti INNER JOIN votipartiti ON partiti.id = votipartiti.partiti WHERE votipartiti.sessioni = ?");
 			ps.setInt(1, sessione.getId());
-			ResultSet rs = ps.executeQuery();
-			
+			rs = ps.executeQuery();
 			while (rs.next()) {
 				res.put(new Partito(rs.getInt("id"), rs.getString("nome")), rs.getInt("nvoti"));
 			}
-			
-			ps.close();
 		} catch (SQLException e) {
 			System.out.println("Errore durante l'ottenimento voti dei candidati della sessione di voto" + sessione.toString());
 			e.printStackTrace();
 		}
-		
+		finally { DbUtils.closeResultSet(rs); DbUtils.closeStatement(ps); }
 		return res;
 	}
 	
-	//aggiunge i partiti per la sessione di voto
+	/**aggiunge il partito per la sessione di voto*/
 	public void addVotiPartitoBySessione(SessioneDiVoto sessione,  Partito partito) {
 		Objects.requireNonNull(sessione);
 		Objects.requireNonNull(partito);
-		int i = 0;
+		PreparedStatement ps = null;
 		try {
-			PreparedStatement ps = connection.prepareStatement("INSERT INTO votipartiti (sessioni, partiti, nvoti) VALUES (?, ?, ?)");
+			ps = connection.prepareStatement("INSERT INTO votipartiti (sessioni, partiti, nvoti) VALUES (?, ?, ?)");
 			ps.setInt(1, sessione.getId());
 			ps.setInt(2, partito.getId());
 			ps.setInt(3, 0);
 			ps.executeUpdate();
-
-			ps.close();
+			System.out.println( "Relazione partiti - sessione di voto " + sessione.getId() + " aggiunta nel database");
 		} catch (SQLException e) {
 			System.out.println("Errore durante l'inserimento del partito  + " + partito.getId() + " per la sessione " + sessione.getId());
 			e.printStackTrace();
-			return;
 		}
-		System.out.println( "Relazione partiti - sessione di voto " + sessione.getId() + " aggiunta nel database");
-		
+		finally {  DbUtils.closeStatement(ps); }
 	}
 	
+	/**elimina tutti i partiti della sessione di voto*/
 	@Override
 	public void deleteVotiPartitiBySessione(SessioneDiVoto sessione) {
 		Objects.requireNonNull(sessione);
 		PreparedStatement ps = null;
-
 		try {
 			ps = connection.prepareStatement("DELETE FROM votipartiti  WHERE sessioni = ?");
 			ps.setInt(1, sessione.getId());
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Partiti per la sessione " + sessione.toString() + " rimossi dal database");
 		} catch (SQLException e) {
 			System.out.println("Errore durante la rimozione dei partiti per la sessione di voto " + sessione.toString());
 			e.printStackTrace();
-			return;
 		}
-		System.out.println("Partiti per la sessione " + sessione.toString() + " rimossi dal database");
+		finally { DbUtils.closeStatement(ps); }
 	}
 }
