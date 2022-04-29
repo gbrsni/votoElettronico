318e569a80c86f3e64c4a1857821c6650fcdfa5a
aless <aless@AleCuccoPc>
2022-04-29 20:43:36 +0200

    tolti package non usati

--- src/main/java/com/gbrsni/votoelettronico/controller/GestoreSessioniController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/GestoreSessioniController.java
@@ -1,23 +1,19 @@
 package com.gbrsni.votoelettronico.controller;
 
-import java.io.IOException;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.ResourceBundle;
-
 import com.gbrsni.votoelettronico.models.Gestore;
 import com.gbrsni.votoelettronico.models.SessioneDiVoto;
-import com.gbrsni.votoelettronico.models.StatoSessione;
-import com.gbrsni.votoelettronico.Home;
+
+
 import com.gbrsni.votoelettronico.data_access.SessioneDiVotoDAOImpl;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -26,11 +22,11 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
+
 
 public class GestoreSessioniController extends Controller{
 	
-	private Gestore gestore ;//= new Gestore("marcox", "marcox","marcox","marcox"); //DA ELIMINARE
+	private Gestore gestore ;
 	private List<SessioneDiVoto> sessioni;
 	
 	@FXML
@@ -149,6 +145,7 @@ public class GestoreSessioniController extends Controller{
         public void handle(ActionEvent e)
         {
             System.out.println("Id Sessione di Voto:" + ((Button)e.getSource()).getId());
+            System.out.println("Scrutinio");
         }
     };
     
