ad8a049315f8da4de554502eba6d2df47f6c5f06
aless <aless@AleCuccoPc>
2022-04-29 20:56:20 +0200

    aggiunta chiusura resultSet e preparedStatement

--- src/main/java/com/gbrsni/votoelettronico/data_access/VotazioniPartitiDAOImpl.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/VotazioniPartitiDAOImpl.java
@@ -13,54 +13,50 @@ import com.gbrsni.votoelettronico.models.SessioneDiVoto;
 
 public class VotazioniPartitiDAOImpl implements VotazioniPartitiDAO {
 	private Connection connection = DBConnection.getConnection();
-
+	
+	/**ottiene tutti i partiti della sessione di voto*/
 	@Override
 	public Map<Partito, Integer> getVotazioniPartitiBySessione(SessioneDiVoto sessioneDiVoto) {
 		Objects.requireNonNull(sessioneDiVoto);
-		
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		Map<Partito, Integer> res = new HashMap<>();
 
 		try {
-			PreparedStatement ps = connection.prepareStatement("SELECT * FROM votazionipartiti WHERE sessioni = ?");
+			ps = connection.prepareStatement("SELECT * FROM votazionipartiti WHERE sessioni = ?");
 			ps.setInt(1, sessioneDiVoto.getId());
-			ResultSet rs = ps.executeQuery();
-			
+			rs = ps.executeQuery();
 			while (rs.next()) {
 				PartitoDAO partitoDb = new PartitoDAOImpl();
 				Partito partito = partitoDb.getPartitoById(Integer.valueOf(rs.getString("partiti")));
 				res.put(partito, rs.getInt("valore"));
 			}
-			
-			ps.close();
 		} catch (SQLException e) {
 			System.out.println("Errore durante l'ottenimento votazioni dei partiti della sessione di voto" + sessioneDiVoto.toString());
 			e.printStackTrace();
 		}
-		
+		finally { DbUtils.closeResultSet(rs); DbUtils.closeStatement(ps); }
 		return res;
 	}
 	
-
+	/**aggiunge la votazione per il partito nella sessione di voto indicata*/
 	@Override
 	public void addVotazioniPartitiBySessione(SessioneDiVoto sessioneDiVoto, Partito partito, int valore) {
 		Objects.requireNonNull(sessioneDiVoto);
 		Objects.requireNonNull(partito);
 		PreparedStatement ps = null;
-		
 		try {
 			ps = connection.prepareStatement("INSERT INTO votazionipartiti (sessioni, partiti, valore) VALUES (?, ?, ?)");
 			ps.setInt(1, sessioneDiVoto.getId());
 			ps.setInt(2, partito.getId());
 			ps.setInt(3, valore);
-		
 			ps.executeUpdate();
-			ps.close();
+			System.out.println("Aggiornate votazioni del partito " + partito.toString() + " per la sessione di voto " + sessioneDiVoto.toString());
+
 		} catch (SQLException e) {
 			System.out.println("Errore durante l'aggiornamento votazioni del partito " + partito.toString() + " per la sessione di voto " + sessioneDiVoto.toString());
 			e.printStackTrace();
-			return;
 		}
-		
-		System.out.println("Aggiornate votazioni del partito " + partito.toString() + " per la sessione di voto " + sessioneDiVoto.toString());
+		finally {DbUtils.closeStatement(ps); }
 	}
 }
