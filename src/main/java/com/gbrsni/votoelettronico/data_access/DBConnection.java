1d2da0b5f36391bd1959925befc90890060d1846
aless <aless@AleCuccoPc>
2022-05-01 13:54:49 +0200

    aggiunta commento

--- src/main/java/com/gbrsni/votoelettronico/data_access/DBConnection.java
+++ src/main/java/com/gbrsni/votoelettronico/data_access/DBConnection.java
@@ -4,20 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-// Questa classe implementa il design patter Singleton
+/**la classe DBConnection implementa il design pattern singleton. Fornisce un metodo get per creare un istanza di connessione con il db*/
 public class DBConnection {
 	private static Connection instance;
 	
 	public static Connection getConnection() {
 		if (instance == null) {
 			try {
-				instance = DriverManager.getConnection("jdbc:Mysql://localhost:3306/test?user=user&password=mynameischef12&serverTimezone=UTC");
+				instance = DriverManager.getConnection("jdbc:mysql://localhost/votoElettronico?user=root&password=admin&serverTimezone=UTC");
+				System.out.println("Connessione al database votoElettoronico effettuata con successo");
 			} catch (SQLException e) {
 				System.out.println("Errore nell'ottenimento della connessione al database");
 				e.printStackTrace();
 			}
 		}
-		
 		return instance;
 	}
 }
