6211fef7b4e8315659945418ddb7635e0daec0cd
aless <aless@AleCuccoPc>
2022-05-04 10:20:41 +0200

    tolta estensione classe OpzioneDiVoto

--- src/main/java/com/gbrsni/votoelettronico/models/SessioneCategorico.java
+++ src/main/java/com/gbrsni/votoelettronico/models/SessioneCategorico.java
@@ -2,6 +2,7 @@ package com.gbrsni.votoelettronico.models;
 
 import java.time.LocalDate;
 
+
 public class SessioneCategorico extends SessioneDiVoto{
 	
 	public SessioneCategorico(int id, String nome, String descrizione, LocalDate data, ModVittoria modVittoria,
@@ -9,9 +10,11 @@ public class SessioneCategorico extends SessioneDiVoto{
 		super(id, nome, descrizione, data, modVittoria, statoSessione, nvoti);
 	}
 	
+	
 	public ModVoto getModVoto() {
 		return ModVoto.CATEGORICO;
 	}
+	
 	
 	public boolean equals(Object obj) {
 		if(this == obj)
@@ -21,4 +24,5 @@ public class SessioneCategorico extends SessioneDiVoto{
 		SessioneCategorico s = (SessioneCategorico) obj;
 		return (s.getId() == this.getId());
 	}
+
 }
