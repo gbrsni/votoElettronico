c89336ccbda85ff6cda1fb5e168cae41e72751ef
aless <aless@AleCuccoPc>
2022-05-04 10:20:00 +0200

    tolte importazioni e metodi non usati

--- src/main/java/com/gbrsni/votoelettronico/models/SessioneDiVoto.java
+++ src/main/java/com/gbrsni/votoelettronico/models/SessioneDiVoto.java
@@ -1,28 +1,10 @@
 package com.gbrsni.votoelettronico.models;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.TreeMap;
 
-import com.gbrsni.votoelettronico.data_access.VotazioniCandidatiDAO;
-import com.gbrsni.votoelettronico.data_access.VotazioniCandidatiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiCandidatiDAO;
-import com.gbrsni.votoelettronico.data_access.VotiCandidatiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiEspressiDAO;
-import com.gbrsni.votoelettronico.data_access.VotiEspressiDAOImpl;
-import com.gbrsni.votoelettronico.data_access.VotiPartitiDAO;
-import com.gbrsni.votoelettronico.data_access.VotiPartitiDAOImpl;
-import com.gbrsni.votoelettronico.exceptions.SessionModVotoException;
-import com.gbrsni.votoelettronico.exceptions.SessionStateException;
 import com.gbrsni.votoelettronico.logging.Logging;
 
-import javafx.util.Pair;
 
 public abstract class SessioneDiVoto {
 	
@@ -58,76 +40,6 @@ public abstract class SessioneDiVoto {
 		return this.id = id;
 	}
 	
-	public void contaVoti() {
-		if (this.statoSessione == StatoSessione.CHIUSA) {			
-			VotazioniCandidatiDAO votazioniCandidatiDAO = new VotazioniCandidatiDAOImpl();
-			List<Pair<Candidato, Integer>> votazioniCandidati = votazioniCandidatiDAO.getVotazioniCandidatiBySessione(this);
-			Map<Candidato, Integer> conteggioCandidati = SessioneDiVoto.getConteggioVoti(votazioniCandidati);
-			
-			VotiCandidatiDAO votiCandidatiDAO = new VotiCandidatiDAOImpl();
-			for (Candidato c : conteggioCandidati.keySet()) {
-				votiCandidatiDAO.addVotiCandidatoBySessione(this, c, conteggioCandidati.get(c));
-			}
-			
-			this.setStatoSessione(StatoSessione.SCRUTINATA);
-		} else {
-			throw new SessionStateException(StatoSessione.CHIUSA, this.statoSessione, this.id);
-		}
-	}
-	
-	public static <O extends OpzioneDiVoto> Map<O, Integer> getConteggioVoti(List<Pair<O, Integer>> voti) {
-		Map<O, Integer> res = new TreeMap<>();
-		
-		for (Pair<O, Integer> voto : voti) {
-			O opzione = voto.getKey();
-			Integer valore = voto.getValue();
-			
-			if (res.containsKey(opzione)) {
-				valore = valore + res.get(opzione);
-			}
-			
-			res.put(opzione, valore);
-		}
-		
-		return res;
-	}
-	
-	protected static <O extends OpzioneDiVoto> List<O> getClassifica(Map<O, Integer> voti) {
-		List<Entry<O, Integer>> l = new ArrayList<>(voti.entrySet());
-		l.sort(Entry.comparingByValue());
-
-		List<O> res = new ArrayList<>(voti.keySet());
-		
-		for (Entry<O, Integer> e : l) {
-			res.add(e.getKey());
-		}
-		
-		Collections.reverse(res);
-		
-		return res;
-	}
-	
-	// Prende in input l'output di getVotiPartitiBySessione
-	public Partito getPartitoVincitore(Map<Partito,Integer> voti) {
-		List<Partito> classifica = SessioneDiVoto.getClassifica(voti);
-		
-		Partito vincitore = classifica.get(0);
-		
-		switch (this.modVittoria) {
-		case MAGGIORANZA:
-			return vincitore;
-		case MAGGIORANZA_ASSOLUTA:			
-			int minVoti = (this.nvoti / 2) + 1;
-			
-			if (voti.get(vincitore) >= minVoti) {
-				return vincitore;
-			} else {
-				return null;
-			}
-		default:
-			throw new SessionModVotoException(this.getModVoto(), this.getId());
-		}
-	}
 
 	public int getId() {
 		return id;
@@ -181,7 +93,24 @@ public abstract class SessioneDiVoto {
 	public void setNvoti(int nvoti) {
 		this.nvoti = nvoti;
 	}
-
+	
+	/*
+	public Map<A,Integer> contaVoti(List<Pair<A,Integer>> votazioni) {
+		if (this.statoSessione == StatoSessione.CONCLUSA) {			
+			Map<A, Integer> res = new TreeMap<A,Integer>();
+			 for (int i = 0; i < votazioni.size(); i++) {
+	               if(res.containsKey(votazioni.get(i).getKey())) {
+	            	   res.replace(votazioni.get(i).getKey(), votazioni.get(i).getValue());
+	               }else {
+	            	   res.put(votazioni.get(i).getKey(), votazioni.get(i).getValue());
+	               }
+	            }
+			return res;
+		} else {
+			throw new SessionStateException(StatoSessione.CONCLUSA, this.statoSessione, this.id);
+		}
+	}
+	*/
 	public abstract ModVoto getModVoto();
 	
 	@Override
