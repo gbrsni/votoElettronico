3b1cb92e24e962664c7fa9c4d3d83c90f7511b58
aless <aless@AleCuccoPc>
2022-05-06 09:27:43 +0200

    aggiunto  null colonna candidati

--- docs/database/dump/votoelettronico_vincitori.sql
+++ docs/database/dump/votoelettronico_vincitori.sql
@@ -24,22 +24,13 @@ DROP TABLE IF EXISTS `vincitori`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `vincitori` (
   `sessioni` int NOT NULL,
-  `candidati` int NOT NULL,
+  `candidati` int DEFAULT NULL,
   PRIMARY KEY (`sessioni`),
   KEY `fk_candidati_idx` (`candidati`),
   CONSTRAINT `fk_candidati_vincitori` FOREIGN KEY (`candidati`) REFERENCES `candidati` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `fk_sessioni_vincitori` FOREIGN KEY (`sessioni`) REFERENCES `sessioni` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vincitori`
---
-
-LOCK TABLES `vincitori` WRITE;
-/*!40000 ALTER TABLE `vincitori` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vincitori` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -50,4 +41,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-04 10:06:55
+-- Dump completed on 2022-05-06  9:26:40
