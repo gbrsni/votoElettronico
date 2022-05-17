4e91997232796eec39ce42dd6bcf279744d6737b
aless <aless@AleCuccoPc>
2022-05-17 14:43:17 +0200

    rimosse label partiti a candidati

--- src/main/java/com/gbrsni/votoelettronico/controller/DashBoardController.java
+++ src/main/java/com/gbrsni/votoelettronico/controller/DashBoardController.java
@@ -25,8 +25,7 @@ public class DashBoardController extends ControllerGestore{
     @FXML
     private Button candidatiButton;
 
-    @FXML
-    private Label candidatiLabel;
+
 
     @FXML
     private Button logoutButton;
@@ -34,8 +33,6 @@ public class DashBoardController extends ControllerGestore{
     @FXML
     private Label nomeGestore;
 
-    @FXML
-    private Label partitiLabel;
 
     @FXML
     private Label sessioniAperteLabel;
@@ -92,10 +89,8 @@ public class DashBoardController extends ControllerGestore{
     @FXML
     void initialize() {
     	 assert candidatiButton != null : "fx:id=\"candidatiBottone\" was not injected: check your FXML file 'DashBoardView.fxml'.";
-         assert candidatiLabel != null : "fx:id=\"candidatiLabel\" was not injected: check your FXML file 'DashBoardView.fxml'.";
          assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'DashBoardView.fxml'.";
          assert nomeGestore != null : "fx:id=\"nomeGestore\" was not injected: check your FXML file 'DashBoardView.fxml'.";
-         assert partitiLabel != null : "fx:id=\"partitiLabel\" was not injected: check your FXML file 'DashBoardView.fxml'.";
          assert sessioniAperteLabel != null : "fx:id=\"sessioniAperteLabel\" was not injected: check your FXML file 'DashBoardView.fxml'.";
          assert sessioniButton != null : "fx:id=\"sessioniBottone\" was not injected: check your FXML file 'DashBoardView.fxml'.";
          assert sessioniChiuseLabel != null : "fx:id=\"sessioniChiuseLabel\" was not injected: check your FXML file 'DashBoardView.fxml'.";
