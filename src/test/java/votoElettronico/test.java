58ccb4295ac64320d29b3d086281ed6b7d75da2e
aless <aless@AleCuccoPc>
2022-05-17 14:59:43 +0200

    prova test push

---
+++ src/test/java/votoElettronico/test.java
@@ -0,0 +1,14 @@
+package votoElettronico;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
